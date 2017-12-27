59908b6c8f6ce2f19c14e8aea5c0fb118595aa0a
kirito96 <eagle1968@live.com>
2017-12-26 23:11:24 -0400

    Switch to lineage

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/TCT/Yaris_M_GSM/device_Yaris_M_GSM.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Yaris_M_GSM
-PRODUCT_NAME := cm_Yaris_M_GSM
+PRODUCT_NAME := lineage_Yaris_M_GSM
 PRODUCT_BRAND := TCT
 PRODUCT_MODEL := Alcatel Pop C2 (4032A)
 PRODUCT_MANUFACTURER := TCT
